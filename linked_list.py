3e9a492c2cb14913001a8248bfd333624dfa5fad
Taery <tae5431@gmail.com>
2025-09-29 23:02:09 +0900

    asdf

---
+++ linked_list.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self,value):
+        self.next=None
+        self.value=value
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.length=1
+    
+    def insert(self,value,index):
+        current=self.head
+        for i in range(index):
+            current=current.next
+        new_node=Node(value)
+        next_node=current.next
+        current.next=new_node
+        new_node.next=next_node
+        self.length+=1
+    
+    def delete(self,index):
+        current=self.head
+        i=0
+        while i<index:
+            before=current
+            current=current.next
+            i+=1
+        before.next=current.next
+        self.length-=1
+        
+    def all_print(self):
+        print(f"Linked list length : {self.length}")
+        current=self.head
+        for i in range(self.length):
+            print(current.value,end=" | ")
+            current=current.next
